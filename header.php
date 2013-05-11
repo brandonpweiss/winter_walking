a552369f1043e23813d856518dc156c61640c350
Brandon Weiss <brandon@bewiseweb.com>
2013-05-11 16:49:34 -0400

    first commit
    first commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+	<nav>
+		<ul>
+			<li><a href="">HOME</a></li>
+			<li><a href="">PRODUCTS</a></li>
+			<li><a href="">ABOUT</a></li>
+			<li><a href="">THE FACTS</a></li>
+			<li><a href="">CONTACT</a></li>
+		</ul>
+	</nav>
+</header>
