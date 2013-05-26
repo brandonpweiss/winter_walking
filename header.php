e29115d35da74bbca484ade816026a6aebd8939d
robcapell <rob@robcapell.com>
2013-05-26 11:51:24 -0400

    added contact form php. need email to email. Will need to be tested on clients server

--- header.php
+++ header.php
@@ -1,8 +1,4 @@
-<?php
-echo '
-
-
-  <header>
+<header>
 	<nav>
 		<ul>
 			<li><a href="index.php">FEATURED</a></li>
@@ -11,12 +7,8 @@ echo '
 			<li><a href="#myModal" role="button" data-toggle="modal">CONTACT</a></li>
 		</ul>
 	</nav>
-</header>';
-?>
+</header>
 
-
-<?php
-echo '
 <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
@@ -27,19 +19,22 @@ echo '
 
 <div class="modal-body">
   <div class="form">
-  <form name="contactform" class="clearfix" id="contactForm" method="post" action="send_form_email.php">
+  <form name="contactform" class="clearfix" id="contactForm" method="post" action="<?$_SERVER['PHP_SELF'];?>">
    	<label>First Name</label>
-    <input  type="text" placeholder="first name" name="first_name" maxlength="80" size="30"><br><br>
-    	<label>Last Name</label>
-    <input  type="text"  placeholder="last name" name="last_name" maxlength="80" size="30"><br><br>
+    <input  type="text" placeholder="first name" name="firstName" maxlength="80" size="30"><br><br>
+    <label>Last Name</label>
+    <input  type="text"  placeholder="last name" name="lastName" maxlength="80" size="30"><br><br>
+    <label>Company</label>
+    <input type="text" name="company" placeholder="company name"><br><br>
+    <label>Phone</label>
+    <input type="text" name="phoneNumber" placeholder="222 333 4455"><br><br>
     	<label>Email</label>
     <input  type="email"  placeholder="youremail@yourdomain.com" name="email" maxlength="80" size="30"><br><br>
     	<label>Inquery</label>
-    <textarea name="comments"  placeholder="comments..." rows="5" cols="20"></textarea><br><br>
+    <textarea name="comments"  placeholder="comments..." rows="2" cols="20"></textarea><br><br>
     <button type="submit" value="Submit" id="submit">SUBMIT</button>
   </form>
   </div>
 
 </div>
-</div>';
-?>
+</div>
