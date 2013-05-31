ad9326c7e52a46aff9242be1ce6a61b3ea831453
Brandon Weiss <brandon@bewiseweb.com>
2013-05-31 11:36:55 -0400

    Revert "Revert "media queries and info page""
    This reverts commit db17e544de0abd68622a348fa1e1f7b40e275910.

--- modal.php
+++ modal.php
@@ -1,48 +1,51 @@
 <?php
 echo '
 
+<style type="text/css">
+body{
+text-align: center;
+padding-top: 20px;
+font-family: GillSansMT;
+}
+img{
+width: 200px;
+}
+p{
+text-align: center;
+}
+form{
+text-align: center;
+font-size: 12px;
+}
+</style>
 
-  <header>
-	<nav>
-		<ul>
-			<li><a href="index.php">FEATURED</a></li>
-			<li><a href="products.php">OUR PRODUCTS</a></li>
-			<li><a href="information.php">OUR COMPANY</a></li>
-			<li id="contact1"><a href="#myModal" role="button" data-toggle="modal">CONTACT</a></li>
-      <li id="contact2"><a href="#" id="footernav">CONTACT</a></li>
-		</ul>
-	</nav>
-</header>';
-?>
-
-
-<?php
-echo '
 <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h1 id="myModalLabel">CONTACT &nbsp;<span class="blue">WINTER WALKING</span></h1>
+    <h3 id="myModalLabel">Modal header</h3>
   </div>
-
-
-
-<div class="modal-body">
+  <div class="modal-body">
+<p>Contact us using the form below:</p><br>
   <div class="form">
   <form name="contactform" class="clearfix" id="contactForm" method="post" action="send_form_email.php">
-   	<label>First Name</label>
+    First Name:
     <input  type="text" placeholder="first name" name="first_name" maxlength="80" size="30"><br><br>
-    	<label>Last Name</label>
+    Last Name:
     <input  type="text"  placeholder="last name" name="last_name" maxlength="80" size="30"><br><br>
-    <label>Company Name</label>
-    <input  type="text"  placeholder="company name" name="last_name" maxlength="80" size="30"><br><br>
-    	<label>Email</label>
+    Email:
     <input  type="email"  placeholder="youremail@yourdomain.com" name="email" maxlength="80" size="30"><br><br>
-    	<label>Comments</label>
+     Date Requested:
+    <input  type="date" id="datepicker" placeholder="Choose a date" name="date"><br><br>
+         Time Requested:
+    <input  type="time" id="timepicker" placeholder="Choose a time" name="time"><br><br>
+    Inquiry:
     <textarea name="comments"  placeholder="comments..." rows="5" cols="20"></textarea><br><br>
     <button type="submit" value="Submit" id="submit">SUBMIT</button>
   </form>
   </div>
-
-</div>
+  <div class="modal-footer">
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+    <button class="btn btn-primary">Save changes</button>
+  </div>
 </div>';
 ?>
