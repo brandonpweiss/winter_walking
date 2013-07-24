d1e441f68420ae42a5f536dc969852e3e31885bb
Brandon Weiss <brandon@bewiseweb.com>
2013-07-24 16:07:52 -0400

    nav stuff
    nav stuff

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 	    	<ul>
 		      <li class="navLeft"><a href="index.php" class="active">HOME</a></li>
 		      <li><a class="hide2" href="featured-product.php">PRODUCTS</a></li>
-		      <li class="hide2 navLogo"><img src="img/shoe.png" alt="Shoe"></li>
+		      <li class="navLogo"><img src="img/shoe.png" alt="Shoe"></li>
 		      <li><a href="information.php">COMPANY</a></li>
 		      <li id="contact1" class="navRight"><a href="#myModal2" role="button" data-toggle="modal">CONTACT</a></li>
 		      <li id="contact2"><a href="#" id="footernav">CONTACT</a></li>
