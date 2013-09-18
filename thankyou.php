9f284ac04ebf2ce22d5a6f322fa879c5ddcd5ee9
Brandon <brandon@raineverywhere.com>
2013-09-18 12:16:22 -0400

    thank you page

---
+++ thankyou.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Winter Walking Product Details</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" href="slider.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="normalize.css">
+	<link rel="stylesheet" href="style.css">
+	<script src="jquery.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/bootstrap-slider.js"></script>
+</head>
+<body>
+	<div class="tops">
+		<div id="logobox">
+  <a href="index.php"><img id="logo" src="img/logowhite.png" alt="Winter Walking we dont just make ice cleats, we perfect them"></a>
+    <div id="tagline">WE DONT JUST MAKE ICE CLEATS, WE PERFECT THEM</div>
+</div>
+<?php include('nav.php');?>
+<?php include('header.php');?>
+<div class="thanks" style="margin-top: 20px; margin-bottom: 40px; text-align: center">
+	<img src="img/bootup.png" alt="Boot Up" style="width: 200px;">
+	<h1 style="color: #666666; text-align: center;">THANK YOU FOR CONTACTING US</h1>
+	<a href="all-products.php">CLICK HERE TO VIEW MORE PRODUCTS ></a>
+</div>
+<?php include('footer.php');?>
+<script src="js/products.js"></script>
+<script src="js/stickynav.js"></script>
+</body>
+</html>
